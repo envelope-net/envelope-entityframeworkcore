62bb7ac6aecde6813a8344b220b557f5bc2688b4
Tomas Pilat <tomas.pilat@disig.sk>
2022-10-02 21:26:52 +0200

    added IDbContextTransactionBehaviorObserverFactory

---
+++ src/Envelope.EntityFrameworkCore/IDbContextTransactionBehaviorObserverFactory.cs
@@ -0,0 +1,9 @@
+﻿using Envelope.Transactions;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Envelope.Database.PostgreSql;
+
+public interface IDbContextTransactionBehaviorObserverFactory
+{
+	ITransactionBehaviorObserver Create(IDbContextTransaction dbContextTransaction, int waitForConnectionExecutingInMilliseconds = 50, int waitForConnectionExecutingCount = 40);
+}
